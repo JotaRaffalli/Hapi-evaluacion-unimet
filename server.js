16087ce9f8e1e2f4fec7ac234da14a70ddeed34e
José Luis Raffalli <raffallijoseluis@gmail.com>
2017-02-01 14:40:30 -0400

    Creación del proyecto
    Node Modules
    Packjage Json
    Carpetas
    Assets
    Plugin Vsion
    Render Engine Ejs

---
+++ server.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const Hapi = require('hapi');
+const Vision = require('vision')
+const Ejs = require('ejs');
+
+// Create a server with a host and port
+const server = new Hapi.Server();
+server.connection({ 
+    host: 'localhost', 
+    port: 3000
+});
+
+//Plugins
+server.register(Vision, function (err) {  
+  if (err) {
+    console.log('Cannot register vision')
+  }
+
+  // configure template support   
+  server.views({
+    engines: {
+      html: Ejs
+    },
+    path: __dirname+'/views'
+  })
+})
+
+
+// Add the route
+server.route({
+    method: 'GET',
+    path:'/test', 
+  	handler: {
+  	  view: 'index'
+  	}
+});
+
+// Start the server
+server.start(function(err) {  
+  if (err) {
+    throw err
+  }
+
+  console.log('Server running at: ', server.info.uri)
+})
