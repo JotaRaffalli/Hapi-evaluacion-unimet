e048596750f45c79a7603bc5ea58e0bffd443b5d
José Luis Raffalli <raffallijoseluis@gmail.com>
2017-03-01 17:10:28 -0430

    Reunion: Color en index, eliminar rutas alternas, local host

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 'use strict';
-// Dependencias ------------------------------------------------------------------------ 
+// Dependencias ------------------------------------------------------------------------
 const Hapi = require('hapi');
 const Routes = require('./lib/routes');
 const Vision = require('vision')
@@ -8,18 +8,18 @@ const Inert = require('inert');
 
 // Crea el servidor --------------------------------------------------------------------
 const server = new Hapi.Server();
-server.connection({ 
+server.connection({
     host: localhost,
     port: 3000
 });
 
 //Plugins ------------------------------------------------------------------------------
-server.register(Vision, function (err) {  
+server.register(Vision, function (err) {
   if (err) {
     console.log('Cannot register vision')
   }
 
-  // Configura el motor de renderizado 
+  // Configura el motor de renderizado
   server.views({
     engines: {
       html: Ejs
@@ -32,52 +32,8 @@ server.register(Vision, function (err) {
 
 server.route(Routes);
 
-// Rutas de prueba : Ver routes.js
-server.route({
-    method: 'GET',
-    path:'/test', 
-  	handler: {
-  	  view: 'index'
-  	}
-});
-
-server.route({
-    method: 'GET',
-    path:'/sign-in', 
-  	handler: {
-  	  view: 'sign-in'
-  	}
-});
-
-
-
-server.route({
-    method: 'GET',
-    path:'/main', 
-    handler: {
-      view: 'main'
-    }
-});
-
-// Archivos Estaticos
-server.register(require('inert'), function(err) {   
-   if (err) {throw err;}
-
-   server.route({   
-     method : 'GET', path : '/public/{path*}', 
-     handler : {      
-       directory : {        
-       path : './public',       
-       listing : false,       
-       index : false      
-       }    
-    }
-  
-   });  
-
-});
 // Levanta el servidor -------------------------------------------------------------------
-server.start(function(err) {  
+server.start(function(err) {
   if (err) {
     throw err
   }
