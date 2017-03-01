32a0eb79af18cad47c3e83e4dbe20614dfead0b5
CaRafa <c.rafa.14@gmail.com>
2017-03-01 16:59:04 -0400

    cambio de puerto a local
    jeje

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const Inert = require('inert');
 // Crea el servidor --------------------------------------------------------------------
 const server = new Hapi.Server();
 server.connection({ 
-    host: process.env.IP,
-    port: process.env.PORT
+    host: localhost,
+    port: 3000
 });
 
 //Plugins ------------------------------------------------------------------------------
